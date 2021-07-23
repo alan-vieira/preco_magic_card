73ac47ed9afe747b8f01d929bcd355fec7639d6a
Alan Vieira <66126584+alan-vieira@users.noreply.github.com>
2021-07-23 12:15:29 -0300

    atualizacao

--- lista_magic_preco.py
+++ lista_magic_preco.py
@@ -20,10 +20,13 @@ valor_medio_srt_lista = []
 valor_medio_lista = []
 valor_final_lista = []
 valor_ml_lista = []
-carta_dic = {}
+cartas_web_dic = {}
 
 # leitura do arquivo excel
-df = pd.read_excel('excel/lista_cartas_magic.xlsx')
+minhas_cartas_df = pd.read_excel('excel/lista_cartas_magic_com_edicao.xlsx')
+
+# eliminar as duplicatas da lista e salvar em um novo dataframe
+cartas_unica_df = minhas_cartas_df.drop_duplicates(subset='nome_portugues', keep="first").reset_index(drop=True)
 
 # algumas configurações para o webdriver
 chrome_options = webdriver.ChromeOptions()
@@ -42,9 +45,10 @@ chrome_options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome(executable_path='c:/chromedriver.exe', options=chrome_options)
 
 # efetuando busca no site
-for i in range(len(df["nome_portugues"])):
+for i in range(len(minhas_cartas_df["nome_portugues"])):
     driver.get("https://www.ligamagic.com/?view=cards/card&card=%s&aux=%s"
-               % (df["nome_ingles"][i], df["nome_portugues"][i]))
+               % (minhas_cartas_df["nome_ingles"][i], minhas_cartas_df["nome_portugues"][i]))
+    sleep(randint(2, 4))
 
     action = ActionChains(driver)
 
@@ -55,16 +59,16 @@ for i in range(len(df["nome_portugues"])):
         try:
             edicao_menu = driver.find_element(By.CSS_SELECTOR, "#edcard_%i > img:nth-child(1)" % edcard)
             action.move_to_element(edicao_menu).click().perform()
-            sleep(randint(2, 3))
+
         except NoSuchElementException:
             break
         else:
             edcard += 1
 
         # pegar o valor médio da carta
-        nome_portugues = df["nome_portugues"][i]
-        nome_ingles = df["nome_ingles"][i]
-        edicao = driver.find_element(By.XPATH, '//*[@id="ed-nome"]').text
+        nome_portugues = cartas_unica_df["nome_portugues"][i]
+        nome_ingles = cartas_unica_df["nome_ingles"][i]
+        edicao = driver.find_element(By.XPATH, '//*[@id="ed-nome"]/a').text
         artista = driver.find_element(By.XPATH, '//*[@id="ed-artista"]/a').text
         raridade = driver.find_element(By.XPATH, '//*[@id="ed-raridade"]/a').text
         valor_medio_srt = driver.find_element(By.XPATH, '//*[@id="card-info"]/div[5]/div[2]/div/div[4]').text
@@ -98,64 +102,29 @@ for i in range(len(df["nome_portugues"])):
         valor_ml_lista.append(valor_ml)
 
 # criação do dicionário
-carta_dic = {'nome_portugues': nome_portugues_lista,
-             'nome_ingles': nome_ingles_lista,
-             'edicao': edicao_lista,
-             'artista': artista_lista,
-             'raridade': raridade_lista,
-             'valor_medio': valor_medio_lista,
-             'valor_ml': valor_ml_lista}
+cartas_web_dic = {'nome_portugues': nome_portugues_lista,
+                  'nome_ingles': nome_ingles_lista,
+                  'edicao': edicao_lista,
+                  'artista': artista_lista,
+                  'raridade': raridade_lista,
+                  'valor_medio': valor_medio_lista,
+                  'valor_ml': valor_ml_lista}
 
 # criação do dataframe
-carta_df = pd.DataFrame().from_dict(carta_dic)
+cartas_web_df = pd.DataFrame().from_dict(cartas_web_dic)
 
+# interseção das duas tabelas
+cartas_final_df = pd.merge(minhas_cartas_df, cartas_web_df, how='left', on=('nome_portugues', 'nome_ingles', 'edicao'))
 
-# Seleção de edições das cartas interessadas
-# leitura do arquivo excel para referência
-cartas_edicao_df = pd.read_excel('excel/lista_cartas_magic_com_edicao.xlsx')
-
-nome_portugues_final_lista = []
-nome_ingles_final_lista = []
-edicao_final_lista = []
-artista_final_lista = []
-raridade_final_lista = []
-valor_medio_final_lista = []
-valor_ml_final_lista = []
-
-for i in range(len(carta_df["nome_portugues"])):
-    for j in range(len(cartas_edicao_df["nome_portugues"])):
-        if (carta_df["nome_portugues"][i]) == (cartas_edicao_df["nome_portugues"][j]) and (
-                carta_df["nome_ingles"][i]) == (cartas_edicao_df["nome_ingles"][j]) and (carta_df["edicao"][i]) == (
-                cartas_edicao_df["edicao"][j]):
-
-            # criação das listas
-            nome_portugues_final_lista.append(carta_df["nome_portugues"][i])
-            nome_ingles_final_lista.append(carta_df["nome_ingles"][i])
-            edicao_final_lista.append(carta_df["edicao"][i])
-            artista_final_lista.append(carta_df["artista"][i])
-            raridade_final_lista.append(carta_df["raridade"][i])
-            valor_medio_final_lista.append(carta_df["valor_medio"][i])
-            valor_ml_final_lista.append(carta_df["valor_ml"][i])
-
-# criação do dicionário final
-carta_final_dic = {'nome_portugues': nome_portugues_final_lista,
-                   'nome_ingles': nome_ingles_final_lista,
-                   'edicao': edicao_final_lista,
-                   'artista': artista_final_lista,
-                   'raridade': raridade_final_lista,
-                   'valor_medio': valor_medio_final_lista,
-                   'valor_ml': valor_ml_final_lista}
-
-# criação do dataframe final
-carta_final_df = pd.DataFrame().from_dict(carta_final_dic)
-
-if len(cartas_edicao_df["nome_portugues"]) == len(carta_final_df["nome_portugues"]):
+# teste se deu tudo certo
+if len(minhas_cartas_df["nome_portugues"]) == len(cartas_final_df["nome_portugues"]):
     print('Ok!')
 else:
     print('Deu errado!')
 
-carta_final_df.to_excel("excel/cartas_magic_output.xlsx")
-print(carta_final_df)
+# geração do arquivo final
+cartas_final_df.to_excel("excel/cartas_magic_output.xlsx")
+print(cartas_final_df)
 
 # fechando o site
 time.sleep(10)
