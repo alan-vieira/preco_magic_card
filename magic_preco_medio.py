72ea03a5ef5381d336ca6553af3666bcb4316ddb
Alan Vieira <66126584+alan-vieira@users.noreply.github.com>
2023-03-17 19:41:15 -0300

    Atualização

--- magic_preco_medio.py
+++ magic_preco_medio.py
@@ -13,12 +13,14 @@ from selenium.common.exceptions import *
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-# leitura do arquivo excel
+
+# transformando o arquivo excel em dataframe
 cartas_df = pd.read_excel('excel/lista_cartas_magic_com_edicao.xlsx')
 
-# eliminar as duplicatas da lista e salvar em um novo dataframe
+# eliminando as duplicatas (cartas com o mesmo nome e edições diferentes)
 cartas_df = cartas_df.drop_duplicates(
     subset='nome_portugues', keep="first").reset_index(drop=True)
+
 
 # algumas configurações para o webdriver
 chrome_options = webdriver.ChromeOptions()
@@ -33,36 +35,34 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--ignore-certificate-errors')
 
-# abrindo o site
+# instanciando a versão do webdriver que será instalada na máquina local
 service = Service(ChromeDriverManager().install())
+
+# instanciando o webdriver
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-nome_portugues_lista = []
-nome_ingles_lista = []
-edicao_lista = []
-artista_lista = []
-raridade_lista = []
-valor_medio_srt_lista = []
-
-cartas_web_dic = []
 
 # efetuando busca no site
-for portugues, ingles in zip(cartas_df['nome_portugues'], cartas_df['nome_ingles']):
+cartas_web_dic = []
 
-    driver.get("https://www.ligamagic.com/?view=cards/card&card=%s&aux=%s" %
-            (portugues, ingles))
+for portugues, ingles in zip(
+                        cartas_df['nome_portugues'], cartas_df['nome_ingles']):
+
+    driver.get(
+        f"https://www.ligamagic.com/?view=cards/card&card= \
+            {portugues}&aux={ingles}")
 
     sleep(randint(2, 4))
 
     action = ActionChains(driver)
 
-    # selecionar a edição
+    # selecionando a edição
     edcard = 0
 
     while True:
         try:
             edicao_menu = driver.find_element(
-                By.CSS_SELECTOR, "#edcard_%i > img:nth-child(1)" % edcard)
+                By.CSS_SELECTOR, f"#edcard_{edcard}> img:nth-child(1)")
             action.move_to_element(edicao_menu).click().perform()
 
         except NoSuchElementException:
@@ -70,7 +70,7 @@ for portugues, ingles in zip(cartas_df['nome_portugues'], cartas_df['nome_ingles
         else:
             edcard += 1
 
-        # pegar o valor médio da carta
+        # pegando algumas características das cartas
         nome_portugues = portugues
         nome_ingles = ingles
         edicao = driver.find_element(By.XPATH, '//*[@id="ed-nome"]/a').text
@@ -80,51 +80,77 @@ for portugues, ingles in zip(cartas_df['nome_portugues'], cartas_df['nome_ingles
         valor_medio_srt = driver.find_element(
             By.XPATH, '//*[@id="card-info"]/div[5]/div[2]/div/div[4]').text
 
-        # conversão do valor médio de string para ponto flutuante #
-        # separa o R$ da string, pega a parte numérica,
-        # troca a vírgula por ponto e convert o resultado em ponto flutuante
-        valor_medio = valor_medio_srt.split(" ")[1].replace(".", "").replace(",", ".")
-        valor_medio = float(valor_medio)
+        # exibindo nome (português e inglês), edição, artista, raridade e o \
+        # valor médio
+        print(f"{edcard}. {nome_portugues} | {nome_ingles} | {edicao} | \
+            {artista} | {raridade} | {valor_medio_srt}")
 
-        # calculo de porcentagem (ml) e valor final do produto
-        if valor_medio >= 79:
-            porcentagem_ml = ((12 / 100) * valor_medio)
-        else:
-            porcentagem_ml = ((12 / 100) * valor_medio) + 5
-
-        valor_ml = round((porcentagem_ml + valor_medio), 2)
-
-        # exibir nome e edição
-        print(f"{edcard}. {nome_portugues} | {nome_ingles} | {edicao} | {artista} | {raridade} | {valor_medio}")
-
-        # criação do dicionário
+        # criando o dicionário
         cartas_web_dic.append({
             'nome_portugues': nome_portugues,
             'nome_ingles': nome_ingles,
             'edicao': edicao,
             'artista': artista,
             'raridade': raridade,
-            'valor_medio': valor_medio,
-            'valor_ml': valor_ml
+            'valor_medio_srt': valor_medio_srt
         })
 
-# criação do dataframe
+
+# convertendo o dicionário para dataframe
 cartas_web_df = pd.DataFrame().from_dict(cartas_web_dic)
 
-# interseção das duas tabelas
+
+# fechando o site
+time.sleep(5)
+driver.close()
+
+
+# transformando o valor médio string para float
+cartas_web_df['valor_medio'] = list(
+    map(lambda x: x.split(" ")[1].replace(".", "").replace(",", "."),
+        cartas_web_df['valor_medio_srt']))
+
+cartas_web_df['valor_medio'] = list(
+    map(lambda x: float(x),
+        cartas_web_df['valor_medio']))
+
+
+# calculando a comissão do mercado livre
+def comissao_ml(valor_medio) -> int:
+    '''
+    calculo de porcentagem (ml) e valor final do produto:
+    - valores maiores ou iguais a R$ 79,00 a comissão do ML é de 11,5%
+    - valores menores que R$ 79,00 a comissão do ML é de 11,5% + R$ 5,00
+    '''
+    if valor_medio >= 79:
+        porcentagem_ml = (0.115 * valor_medio)
+    else:
+        porcentagem_ml = (0.115 * valor_medio) + 5
+
+    valor_ml = round((porcentagem_ml + valor_medio), 2)
+
+    return valor_ml
+
+
+cartas_web_df['valor_ml'] = list(
+    map(lambda x: comissao_ml(x),
+        cartas_web_df['valor_medio']))
+
+
+# interseção das duas tabelas (para retornar apenas as edições de interesse)
 cartas_final_df = pd.merge(cartas_df, cartas_web_df, how='left', on=(
     'nome_portugues', 'nome_ingles', 'edicao'))
 
-# teste se deu tudo certo
+
+# teste para saber se foi realmente retoranda a lista desejada
 if len(cartas_df["nome_portugues"]) == len(cartas_final_df["nome_portugues"]):
     print('Ok!')
 else:
     print('Deu errado!')
 
-# geração do arquivo final
-cartas_final_df.to_excel("excel/cartas_magic_output.xlsx")
-print(cartas_final_df)
 
-# fechando o site
-time.sleep(10)
-driver.close()
+# gerando o arquivo final
+cartas_final_df.to_excel("excel/cartas_magic_output.xlsx")
+
+# exibindo as 15 cartas mais caras 
+print(cartas_final_df.sort_values(by='valor_medio', ascending=False).head(15))
